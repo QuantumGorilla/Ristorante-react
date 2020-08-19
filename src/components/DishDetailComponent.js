fde9b12ddff56f1a568d1a7c1ea27ca420429670
QuantumGorilla <manotasja@uninorte.edu.co>
2020-08-18 23:00:47 -0500

    Redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -26,7 +26,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
         }
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if(comments!==null){
             return (
                 <div className="col-12 col-md-5 m-1">
@@ -41,7 +41,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                             );   
                         })}
                     </ul>
-                    <CommentForm/>
+                    <CommentForm dishId = {dishId} addComment = {addComment}/>
                 </div>
             )
             } else {
@@ -67,7 +67,9 @@ const minLength = (len) => (val) => val && (val.length >= len);
                 </div>
                 <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId = {props.dish.id}/>
                 </div>
                 </div>
             );
@@ -99,8 +101,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values){
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     };
 
     render(){
