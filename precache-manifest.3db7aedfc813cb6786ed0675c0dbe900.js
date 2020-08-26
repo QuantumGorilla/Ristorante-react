dc70d27bf594588462d4d939a2732b8bc8fd4fe2
QuantumGorilla <manotasja@uninorte.edu.co>
2020-08-26 14:23:23 -0500

    Updates

---
+++ precache-manifest.3db7aedfc813cb6786ed0675c0dbe900.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca62f69e8bb223fac85905c27bd7307e",
+    "url": "/Ristorante-react/index.html"
+  },
+  {
+    "revision": "586f6504a4a08655157f",
+    "url": "/Ristorante-react/static/css/2.321ee481.chunk.css"
+  },
+  {
+    "revision": "8a1ce18f7b7dbf5100fe",
+    "url": "/Ristorante-react/static/css/main.4830df2f.chunk.css"
+  },
+  {
+    "revision": "586f6504a4a08655157f",
+    "url": "/Ristorante-react/static/js/2.9276b3fe.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Ristorante-react/static/js/2.9276b3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1ce18f7b7dbf5100fe",
+    "url": "/Ristorante-react/static/js/main.ffb95c33.chunk.js"
+  },
+  {
+    "revision": "a3f614ef552174bdfd37",
+    "url": "/Ristorante-react/static/js/runtime-main.c17dcf60.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
