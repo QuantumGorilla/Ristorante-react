7ab8cc1f51571de5ce0e2ebc2a917d26d9407ff0
QuantumGorilla <manotasja@uninorte.edu.co>
2020-08-26 22:19:15 -0500

    Ez deployment

---
+++ json-server/public/precache-manifest.c7f7ee0c4cd9b3d49b19ae0f1e5bd09c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193c0a4421defcc35141050055982e18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37701785eac620b705e9",
+    "url": "/static/css/2.0f7fbfd3.chunk.css"
+  },
+  {
+    "revision": "605b8841fda5ff9a330c",
+    "url": "/static/css/main.4830df2f.chunk.css"
+  },
+  {
+    "revision": "37701785eac620b705e9",
+    "url": "/static/js/2.9276b3fe.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.9276b3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605b8841fda5ff9a330c",
+    "url": "/static/js/main.062be695.chunk.js"
+  },
+  {
+    "revision": "7356d298505eec143204",
+    "url": "/static/js/runtime-main.06f63e2e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
